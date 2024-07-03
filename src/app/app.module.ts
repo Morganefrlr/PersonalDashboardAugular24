c20cd0f866d1b11456729a5ca5e67f8a26fbc2e8
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-07-03 15:27:16 +0200

    add ToDoList module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoListModule } from './to-do-list/to-do-list.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToDoListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
