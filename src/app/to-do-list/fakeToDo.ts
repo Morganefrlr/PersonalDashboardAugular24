30886e22e61554d3e293e9249589a3d659964ea0
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-07-08 11:15:17 +0200

    01: + fakeData

---
+++ src/app/to-do-list/fakeToDo.ts
@@ -0,0 +1,25 @@
+export type toDo = {
+    id: number;
+    title: string;
+}
+
+
+export const FAKETODOS: toDo[] = [
+    {
+        id: 1,
+        title: "Meeting Room 201",
+    },
+    {
+        id: 2,
+        title: "Reply to my emails",
+    },
+    {
+        id: 3,
+        title: "Yoga, 30 minutes",
+    },
+    {
+        id: 4,
+        title: "Create a new project, brainstorming 1 hour",
+
+    },
+]
