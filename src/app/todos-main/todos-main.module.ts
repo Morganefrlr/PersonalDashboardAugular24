a09840b07a3e989c8fbad1177a74bc84277101b8
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-07-22 11:43:27 +0200

    add todos & style

--- src/app/todos-main/todos-main.module.ts
+++ src/app/todos-main/todos-main.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { TodosComponent } from './todos/todos.component';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 
 const todosRoutes: Routes = [
   {path:"todos", component: TodosComponent},
+  {path:"todos/add", component: AddTodoComponent},
 ]
 
 @NgModule({
   declarations: [
     TodosComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     CommonModule,
