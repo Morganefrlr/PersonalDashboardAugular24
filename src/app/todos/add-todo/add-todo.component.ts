72de918f26f555804733f9e29053327864c61da4
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-07-25 10:45:51 +0200

    add a todo

--- src/app/todos/add-todo/add-todo.component.ts
+++ src/app/todos/add-todo/add-todo.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodosService } from '../todos.service';
+import { Todo } from '../todoModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
   styles: ``
 })
-export class AddTodoComponent {
+export class AddTodoComponent implements OnInit{
+   
+  value: string
+constructor(
+  private todosService : TodosService,
+  private router : Router
+){}
+  ngOnInit(): void {
+    
+  }
 
+  onSubmit(){
+    this.todosService.addTodo(new Todo(this.value))
+    this.router.navigate(['/todos'])
+  }
 }
