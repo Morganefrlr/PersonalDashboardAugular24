3d0cbb6064a21672c69c4f3eae3cd9989664db0a
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-07-29 15:19:37 +0200

    add note

--- src/app/notes/add-note/add-note.component.ts
+++ src/app/notes/add-note/add-note.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-note',
   template: `
-  
+   <app-form
+   [title]="title"
+   ></app-form>
   `
 })
-export class AddNoteComponent {
+export class AddNoteComponent implements OnInit{
+  title : string = "Ajouter une Note"
 
+  ngOnInit(): void {
+    
+  }
 }
