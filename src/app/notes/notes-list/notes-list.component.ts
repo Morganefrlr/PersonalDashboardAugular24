bb047c283331158115b0f30660a25a2889447b8a
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-07-31 09:47:16 +0200

    delete note animation

--- src/app/notes/notes-list/notes-list.component.ts
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -2,11 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Note } from '../noteModel';
 import { NotesService } from '../notes.service';
 import { Router } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes-list.component.html',
-  styles: ``
+  animations: [
+    trigger('deleteAnimation', [
+      transition(':leave', [
+        animate(200, style({
+          opacity: 0,
+          height: 0,
+          marginBottom: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit{
 
