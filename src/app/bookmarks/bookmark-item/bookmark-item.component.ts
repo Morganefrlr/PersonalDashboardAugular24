620c2ea02193d2881be78784e0ee79d07059648d
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-08-14 10:00:57 +0200

    book item

---
+++ src/app/bookmarks/bookmark-item/bookmark-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmark-item',
+  templateUrl: './bookmark-item.component.html',
+  styles: ``
+})
+export class BookmarkItemComponent {
+
+}
--- src/app/bookmarks/bookmarks.module.ts
+++ src/app/bookmarks/bookmarks.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookmarksListComponent } from './bookmarks-list/bookmarks-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarkItemComponent } from './bookmark-item/bookmark-item.component';
 
 const bookmarksRoutes: Routes = [
   {path:"bookmarks", component: BookmarksListComponent},
@@ -12,7 +13,8 @@ const bookmarksRoutes: Routes = [
 
 @NgModule({
   declarations: [
-    BookmarksListComponent
+    BookmarksListComponent,
+    BookmarkItemComponent
   ],
   imports: [
     CommonModule,
