8337ec182294987efc8d0ae7c860245b3b43984e
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-08-20 10:14:18 +0200

    bookmarks crud service

--- src/app/bookmarks/bookmarks.service.ts
+++ src/app/bookmarks/bookmarks.service.ts
@@ -16,7 +16,25 @@ export class BookmarksService {
   getBookmarks(){
     return this.bookmarks
   }
-  addBookmark(){
 
+
+  addBookmark(bookmark : Bookmark){
+    this.bookmarks.push(bookmark)
+  }
+
+  getBookmarkById(id: number){
+    return this.bookmarks.find(el => el.id === id)
+  }
+
+  updateBookmark(bookmark : Bookmark){
+    const bookmarkToUpdate: Bookmark | any = this.getBookmarkById(bookmark.id)
+    const bookmarkIndex : number = this.bookmarks.findIndex(el => el.id === bookmark.id)
+
+    this.bookmarks[bookmarkIndex] = bookmarkToUpdate
+  }
+
+  deleteBookmark(id : number){
+    const bookmarksUpdated = this.bookmarks.filter(el => el.id !== id)
+    this.bookmarks = bookmarksUpdated
   }
 }
