8ff08147d2d00ddb26dd848b59ee0aadec43826c
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-08-28 10:39:04 +0200

    getR

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { catchError, Observable, of, tap } from 'rxjs';
 import { Recette } from '../recipeModel';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -9,27 +10,16 @@ import { Recette } from '../recipeModel';
 })
 export class RecipesListComponent implements OnInit{
 
-  api  = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+
   
   recettes : Recette[]
 
-  constructor(private http : HttpClient){}
+  constructor(private recipesService : RecipesService){}
 
   ngOnInit(){
-    this.getRecette().subscribe(res => console.log(res.meals))
+    this.recipesService.getRecipes().subscribe(res => console.log(res))
 
   }
 
-  getRecette():Observable<any>{
-    return this.http.get<any>(this.api)
-    .pipe(
-      tap((res) => console.log(res)),
-      catchError((error) => this.handleError(error, []))
-    )
-  }
-
-  private handleError(error : Error, errorValue : any){
-    console.error(error)
-    return of(errorValue)
-  }
+ 
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
 
-  constructor() { }
+  url  = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+ 
+  constructor(private http : HttpClient) { }
+
+
+
+  getRecipes():Observable<any>{
+    return this.http.get<any>(this.url)
+    .pipe(
+      tap((res) => console.log(res)),
+      catchError((error) => this.handleError(error, []))
+    )
+  }
+
+
+
+
+  private log(response: any ){
+    console.table(response)
+  }
+
+  private handleError(error : Error, errorValue : any){
+    console.error(error)
+    return of(errorValue)
+  }
+
 }
