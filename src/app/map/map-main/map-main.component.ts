d1d07e06729621f1c41a115b8bc641169c982cb6
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-09-23 15:22:48 +0200

    map test

--- src/app/map/map-main/map-main.component.ts
+++ src/app/map/map-main/map-main.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
 
 @Component({
   selector: 'app-map-main',
   templateUrl: './map-main.component.html',
 })
-export class MapMainComponent {
+export class MapMainComponent implements OnInit{
 
+  map: any
+
+  ngOnInit() {
+    this.mapConfig()
+  }
+
+
+
+  mapConfig(){
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+  
+  }
 }
