7e89b7447bb59854efe32eb522ac82a132a76dfb
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-09-23 14:42:01 +0200

    + map style first page routes

--- src/app/map/map-main/map-main.component.ts
+++ src/app/map/map-main/map-main.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-map-main',
-  template: `
-    <p>
-      map-main works!
-    </p>
-  `,
-  styles: ``
+  templateUrl: './map-main.component.html',
 })
 export class MapMainComponent {
 
--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapMainComponent } from './map-main/map-main.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const mapRoutes: Routes = [
+  {path:"map", component: MapMainComponent, data: {tab: 5}},
+]
 
 @NgModule({
   declarations: [
     MapMainComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(mapRoutes)
   ],
   providers: [
     MapModule,
